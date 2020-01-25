f54bd3563d106b60db5ef8f3464655551ba9e9bd
Ethan Davidson <theejdavidson@gmail.com>
2020-01-25 20:25:52 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,7 +42,7 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 classification = {
-   PORTION_1
+   PORTION_1 => PORTION_2
 }
 classification
 end
